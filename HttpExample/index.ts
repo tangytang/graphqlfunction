4aac597a9042abae2c6e247127b9c3b674e925dd
tangytang <tangjingyuanivan@gmail.com>
2022-08-20 20:39:12 +0900

    remove graph

--- HttpExample/index.ts
+++ HttpExample/index.ts
@@ -1,12 +1,11 @@
 import  { AzureFunction, Context, HttpRequest } from "@azure/functions"
-const main = require("graph")
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : main();
+        : "hello world"
 
     context.res = {
         // status: 200, /* Defaults to 200 */
